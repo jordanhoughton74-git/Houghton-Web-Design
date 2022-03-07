7cea963b52d8eca63a585b11658470a9759c407a
Jordan Houghton <jordanhoughton74@gmail.com>
2022-03-07 23:58:25 +0000

    raise css priority

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/globals.css'
 import Head from 'next/head';
 import Header from "../components/header"
 import Footer from "../components/footer"
@@ -5,9 +7,6 @@ import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import '../styles/globals.css'
 
 
 function MyApp({ Component, pageProps }) {
